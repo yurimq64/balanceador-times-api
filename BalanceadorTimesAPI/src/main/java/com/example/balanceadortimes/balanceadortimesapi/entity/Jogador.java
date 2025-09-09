484b2510b4ff5dd8d7050276056eb011a19eee81
yuri <ymendesqueiroz@gmail.com>
2025-09-09 19:35:00 -0300

    feat: Adiciona entidade Jogador

---
+++ BalanceadorTimesAPI/src/main/java/com/example/balanceadortimes/balanceadortimesapi/entity/Jogador.java
@@ -0,0 +1,49 @@
+package com.example.balanceadortimes.balanceadortimesapi.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Jogador {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private int nota = 0;
+
+    public Jogador() {
+    }
+
+    public Jogador(String nome, int nota) {
+        this.nome = nome;
+        this.nota = nota;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getNota() {
+        return nota;
+    }
+
+    public void setNota(int nota) {
+        this.nota = nota;
+    }
+}
