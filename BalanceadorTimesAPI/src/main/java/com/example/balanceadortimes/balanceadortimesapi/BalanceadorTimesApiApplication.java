66116f512b8a3773b83ae9cd22912cdfae2f0776
yuri <ymendesqueiroz@gmail.com>
2025-09-09 16:01:30 -0300

    Initial commit: Configuração inicial do projeto Spring com MySQL

---
+++ BalanceadorTimesAPI/src/main/java/com/example/balanceadortimes/balanceadortimesapi/BalanceadorTimesApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.balanceadortimes.balanceadortimesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BalanceadorTimesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BalanceadorTimesApiApplication.class, args);
+	}
+
+}
---
+++ BalanceadorTimesAPI/src/test/java/com/example/balanceadortimes/balanceadortimesapi/BalanceadorTimesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.balanceadortimes.balanceadortimesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BalanceadorTimesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
